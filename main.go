20e07032564cb231c34a0240f4f7f20a7034b5e7
Sytron-dev <135658728+Sytron-dev@users.noreply.github.com>
2023-06-10 21:28:12 +0300

    New updates

--- main.go
+++ main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"log"
 )
-func main() {
-	http.HandleFunc("/hello-world", func (w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+func confHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/conf" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "conf!")
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
 
-	})
-	http.ListenAndServe(":8080", nil)
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err:= http.ListenAndServe(":8080", nil); err !=nil {
+		log.Fatal(err)
+	}
 }
