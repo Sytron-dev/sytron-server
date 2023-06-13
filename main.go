da15e9f68d0ab8a2267d19be9eeb053686e0f59c
Sytron-dev <135658728+Sytron-dev@users.noreply.github.com>
2023-06-13 09:06:32 +0300

    Routing

--- main.go
+++ main.go
@@ -1,29 +1,74 @@
 package main
 
-// import (
-// 	"net/http"
-// 	"fmt"
-// 	"log"
-// 	"github.com/nedpals/supabase-go"
-// )
-import (	
+import (
+	"fmt"
+	"net/http"
 	"log"
-	"github.com/joho/godotenv"
+	"github.com/google/uuid"
+	"github.com/nedpals/supabase-go"
 	supa "github.com/nedpals/supabase-go"
 ) 
 
-type Country struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Capital string `json:"capital"`
-  }
 
-func main(){
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+type User struct {
+	ID uuid.UUID `json:"id"`
+	First_name string `json:"first_name"`
+	Last_name string `json:"last_name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+func updateUser(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/updateuser" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
 	}
-	supabaseUrl := "supabaseUrl"
-	supabaseKey := "supabaseKey"
+	row := User{
+		First_name: "Vincent",
+		Last_name: "Kamemia",
+		Email: "vincentkamemia@gmail.com",
+		Password: "12345",
+	}
+	var results map[string]interface{}
+	err := supabase.DB.From("users").Update(row).Execute(&results)
+	if err != nil {
+		panic(err)
+	  }
+	  fmt.Println(results)
+}
+func createUser(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/createuser" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	supabaseUrl := "https://bnzcbbpmekiavacefqfr.supabase.co"
+	supabaseKey := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuemNiYnBtZWtpYXZhY2VmcWZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY0Nzc1MjYsImV4cCI6MjAwMjA1MzUyNn0.MYAFfpmn2xo6OVoYgMDYyQuNBzpwfEvHaU1w8eF8VJg"
 	supabase := supa.CreateClient(supabaseUrl, supabaseKey)
+	// Generate a new UUID
+	
+	// Create a new user
+	row := User{
+		ID : uuid.New(),
+		First_name: "Vincent",
+		Last_name: "Kamemia",
+		Email: "johndoe@mail.com",
+		Password: "password",
+		}
+		var results []User
+		err := supabase.DB.From("users").Insert(row).Execute(&results)
+		if err != nil {
+			panic(err)
+		  }
+
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/createuser", createUser)
+	http.HandleFunc("/updateuser", updateUser)
+	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting server at port 8080\n")
+	if err:= http.ListenAndServe(":8080", nil); err !=nil {
+		log.Fatal(err)
+	}
+
 }
