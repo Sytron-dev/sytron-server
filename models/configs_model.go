23565226d63462359c35368fd34155828530700c
TallestLegacy <tallestlegacy@gmail.com>
2023-07-12 17:47:28 +0300

    updated naming convention

---
+++ models/configs_model.go
@@ -0,0 +1,32 @@
+package models
+
+import "github.com/google/uuid"
+
+type Country struct {
+	ID           uuid.UUID `json:"id"`
+	Value        string    `json:"value"`
+	Label        string    `json:"label"`
+	Lat          float32   `json:"lat"`
+	Lon          float32   `json:"lon"`
+	Emoji        string    `json:"emoji"`
+	CurrencyID   string    `json:"currency_id"`
+	CurrencyName string    `json:"currency_name"`
+}
+
+type Location struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Lat       float32   `json:"lat"`
+	Lon       float32   `json:"lon"`
+	CountryID string    `json:"country_id"`
+}
+
+type Accomodation struct {
+	ID 		uuid.UUID `json:"id"`
+	Name 	string `json:"name"`
+	Price 	float32 `json:"price"`
+	LocationID string `json:"location_id"`
+	Rating 	float32 `json:"rating"`
+	Rooms 	int `json:"rooms"`
+	Description string `json:"description"`
+}
